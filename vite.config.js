35b9603d25fad27e037799e97937039ae425f964
bcns <1787295782@qq.com>
2024-04-01 20:54:42 +0800

    vite 配置

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,61 @@
-import { defineConfig } from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
-import vue from '@vitejs/plugin-vue'
-//引入自动引入插件
+import createVitePlugins from './vite/plugins'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  // base:'/admin/',
-  plugins: [vue(), AutoImport({
-    resolvers: [ElementPlusResolver()],
-    imports: [
-      'vue',
-      'vue-router',
-      'pinia'
-    ]
-  }),
-  createSvgIconsPlugin({
-    iconDirs: [path.resolve(process.cwd(), 'src/assets/icons/svg')],
-    symbolId: 'icon-[dir]-[name]',
-  }),
-  Components({
-    resolvers: [ElementPlusResolver()],
-  }),],
-  // base:"/app/",
-  resolve: {
-    // https://cn.vitejs.dev/config/#resolve-alias
-    alias: {
-      // 设置路径
-      '~': path.resolve(__dirname, './'),
-      // 设置别名
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_APP_ENV } = env
+  return {
+    // 部署生产环境和开发环境下的URL。
+    // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
+    // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
+    base: VITE_APP_ENV === 'production' ? '/' : '/',
+    plugins: createVitePlugins(env, command === 'build'),
+    resolve: {
+      // https://cn.vitejs.dev/config/#resolve-alias
+      alias: {
+        // 设置路径
+        '~': path.resolve(__dirname, './'),
+        // 设置别名
+        '@': path.resolve(__dirname, './src')
+      },
+      // https://cn.vitejs.dev/config/#resolve-extensions
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
     },
-    // https://cn.vitejs.dev/config/#resolve-extensions
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  },
-  define: {
-    __NEXT_NAME__: JSON.stringify(process.env.npm_package_name),
-  },
-  // vite 相关配置
-  server: {
-    port: 8091,
-    host: true,
-    proxy: {
-      // https://cn.vitejs.dev/config/#server-proxy
-      '/dev-api': {
-        target: 'http://154.83.14.233:7777',
-        changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/dev-api/, '')
+    define: {
+      __NEXT_NAME__: JSON.stringify(process.env.npm_package_name),
+    },
+    // vite 相关配置
+    server: {
+      port: 8091,
+      host: true,
+      open: true,
+      proxy: {
+        // https://cn.vitejs.dev/config/#server-proxy
+        '/dev-api': {
+          target: 'http://154.83.14.233:7777',
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/dev-api/, '')
+        }
+      }
+    },
+    //fix:error:stdin>:7356:1: warning: "@charset" must be the first rule in the file
+    css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
       }
     }
-  },
+  }
 })
